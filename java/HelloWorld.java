6a277e71a8d3a47b583ebf8469b5b4e3ba8ab80c
marvelrock <thuang1020@163.com>
2016-06-30 10:17:57 +0800

    我的java hello world

---
+++ java/HelloWorld.java
@@ -0,0 +1,13 @@
+// 我的第一个java程序
+//
+// java实现文件的后缀是.java
+// 需要保证class名与文件名保持一致
+//
+// javac HelloWorld.java
+// java HelloWorld
+
+public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("hello world");
+    }
+}
